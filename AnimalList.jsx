c63022cb361f068359f52162d773166424569642
fitrianurfa <93827037+fitrianurfa@users.noreply.github.com>
2023-05-13 00:58:51 +0700

    fiks ubah gambar

---
+++ AnimalList.jsx
@@ -0,0 +1,96 @@
+import './animalList.css'
+import './animalList.css'
+import { useNavigate } from 'react-router';
+import { useEffect, useState } from 'react';
+import { collection, getDocs} from 'firebase/firestore';
+import { db } from '../../../config/firebase/index';
+import Footer from '../../../components/Footer';
+import {bottom_wave} from './assets/images'
+
+function AnimalList () {
+  const [animals,setanimals] = useState([]);
+  const animalsCollectionRef = collection(db,"animals");
+  const navigate = useNavigate();
+  const navigateToLandingPage = () => {
+      navigate('/');
+};
+
+  const [filter, setFilter] = useState('');
+  const searchText = (event) =>{
+    setFilter(event.target.value);
+  }
+  let dataSearch = animals.filter(item=>{
+    
+    return Object.keys(item).some(key =>
+      item[key].toString().toLowerCase().includes(filter.toString().toLowerCase())
+      )
+  });
+
+  class ShowData{
+    constructor(collectionRef){
+      this.Ref = collectionRef
+    }
+    async show(){
+      this.data = await getDocs(this.Ref)
+      setanimals(this.data.docs.map(
+        (doc)=>(
+          {...doc.data(),id: doc.id}
+        )
+      ))
+    }
+  }
+
+  // READ
+  useEffect(()=>{
+    const getanimals = async()=>{
+      const newData = new ShowData(animalsCollectionRef)
+      await newData.show()
+    }
+    getanimals();  
+  },[])
+
+  return (
+    <>
+      <header>
+        <a className="logo" onClick={navigateToLandingPage}>
+          {" "}
+          <i className="fas fa-paw" /> Zootopia
+        </a>
+        <nav className="navbar">
+        </nav>
+        <form  >
+            <input
+              className="search"
+              type="text"
+              placeholder="Cari Hewan"
+              onChange={searchText.bind(this)}
+              value={filter}
+            />
+          </form>
+      </header>
+      <section className="home1" id="home">
+        <img src={bottom_wave} className="wave" />
+      </section>
+      <section className="data">
+        <h2 className="judul">DATA HEWAN</h2>
+        <div className="flex-box">
+        {dataSearch.map((animal,pos)=>{
+         return (
+          <div className="card" key={pos} > 
+          <img src={animal.img}></img>
+            <h2 >{animal.name} </h2>
+            <p className="">Scientific Name : {animal.species} </p>          
+            <p className="">Class :  {animal.classification} </p>
+            <p className="">Food : {animal.food} </p> 
+            <p className="">Habitat : {animal.environment} </p>
+            <p className="">Characteristics : {animal.description} </p>
+          </div>
+          )})}  
+        </div>
+      </section>
+      <Footer />
+    </>
+  )
+}
+
+export default AnimalList
---
+++ Login.jsx
@@ -0,0 +1,85 @@
+import React, {useState} from 'react';
+import './login.css'
+import 'material-symbols'
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../../config/firebase';
+import { useAtom, atom } from 'jotai';
+import { useNavigate} from 'react-router-dom';
+import { useEffect } from 'react';
+
+export const userAtom = atom(localStorage.getItem('userEmail') ?? null);
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useAtom(userAtom);
+  
+  const navigate = useNavigate();
+  const navigateToAnimalForm = ()=> {
+    navigate('/AnimalForm'); }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential)=>{
+      setUser(userCredential.user.email.toLowerCase());
+      localStorage.setItem('userEmail', userCredential.user.email.toLowerCase());
+      const user = userCredential.user.email.toLowerCase();
+      console.log(userCredential);   
+      console.log(user);
+      alert("login succes")
+      navigateToAnimalForm()
+    }).catch((error)=> {
+      console.log(error)
+      alert("login failed")
+    })
+  }
+
+useEffect(()=>{
+},[]);
+
+
+  return (
+    <div className="login-card-container">
+      <div className="login-card">
+        <div className="login-card-logo">
+          <img src="/src/assets/images/logo.png" alt="logo" className="image"/>
+        </div>
+        <div className="login-card-header">
+          <h1>Login</h1>
+          <div>for Admin Only</div>
+        </div>
+        <form onSubmit={handleSubmit} className="login-card-form">
+          <div className="form-item">
+            <span className="form-item-icon material-symbols-rounded">person</span>
+            <input
+              type="text"
+              placeholder="Masukkan Username"
+              id="emailForm"
+              autoFocus=""
+              required=""
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="form-item">
+            <span className="form-item-icon material-symbols-rounded">lock</span>
+            <input
+              type="password"
+              placeholder="Masukkan Password"
+              id="passwordForm"
+              required=""
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button type='submit'>login</button>
+          </div>        
+        </form>
+      </div>
+    </div>
+  )
+}
+
+
+
+export default Login;
