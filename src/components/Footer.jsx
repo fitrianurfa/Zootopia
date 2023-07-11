d68fe83f44cf3bd985091941031f501e67baa499
Fitria Nur Fatmawati <93827037+fitrianurfa@users.noreply.github.com>
2023-07-12 06:55:11 +0700

    chore: Update icon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,13 @@
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPaw } from '@fortawesome/free-solid-svg-icons'
+import { faClock } from '@fortawesome/free-solid-svg-icons'
+import { faPhoneAlt } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import { faFacebook } from '@fortawesome/free-brands-svg-icons'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faTwitter} from '@fortawesome/free-brands-svg-icons'
 
 function Footer () {
   return(
@@ -6,51 +15,53 @@ function Footer () {
       <div className="box-container">
         <div className="box">
           <h3>
-            <i className="fas fa-paw" /> zoo
+          <FontAwesomeIcon icon={faPaw} style={{color: "#ff7300",}} /> zoo
           </h3>
           <p >Zoo Open Schedule</p>
           <p className="links" >
-            <i className="fas fa-clock" />
-            Tuesday - Sunday
+          <FontAwesomeIcon icon={faClock} style={{color: "#ff7300",}} /> Tuesday - Sunday
           </p>
           <p className="days">8:00AM - 11:00PM</p>
         </div>
         <div className="box">
           <h3>Contact Info</h3>
           <a href="#" className="links">
-            <i className="fas fa-phone" /> 1245-147-2589
+          <FontAwesomeIcon icon={faPhoneAlt} style={{color: "#ff7300",}} /> 1245-147-2589
           </a>
-          <a  href="#" className="links">
-            <i className="fas fa-envelope" /> information@zootopia.com
+          <a href="#" className="links">
+            <FontAwesomeIcon icon={faEnvelope} style={{color: "#ff7300",}} /> information@zootopia.com
           </a>
-          <a  href="#" className="links">
-            <i className="fas fa-map-marker-alt" /> boyolali, indonesia
+          <a href="#" className="links">
+          <FontAwesomeIcon icon={faMapMarkerAlt} style={{color: "#ff7300",}} /> boyolali, indonesia
           </a>
         </div>
         <div className="box">
           <h3>quick links</h3>
-          <a href="#" className="links">
-            {" "}
-            <i className="fas fa-arrow-right" />
+          <a href="#home" className="links">
+            <FontAwesomeIcon icon={faArrowRight} style={{color: "#ff7300",}} />
             home
           </a>
-          <a  href="#" className="links">
-            {" "}
-            <i className="fas fa-arrow-right" />
+          <a  href="#about" className="links">
+            <FontAwesomeIcon icon={faArrowRight} style={{color: "#ff7300",}} />
             about
           </a>
-          <a  href="#" className="links">
-            {" "}
-            <i className="fas fa-arrow-right" />
+          <a  href="#animals" className="links">
+            <FontAwesomeIcon icon={faArrowRight} style={{color: "#ff7300",}} />
             animals
           </a>
         </div>
         <div className="box">
           <h3>Social Media</h3>
           <div className="share">
-            <a href="#" className="fab fa-facebook-f" />
-            <a href="#" className="fab fa-twitter" />
-            <a href="#" className="fab fa-instagram" />
+            <a href="#" >
+            <FontAwesomeIcon icon={faFacebook} style={{color: "#ffff"}} size="2x"/>
+             </a>
+            <a href="#" >
+            <FontAwesomeIcon icon={faTwitter} style={{color: "#ffff",}} size="2x" /> 
+            </a>
+            <a href="#" >
+            <FontAwesomeIcon icon={faInstagram} style={{color: "#ffff",}} size="2x"/>
+             </a>
           </div>
         </div>
       </div>
